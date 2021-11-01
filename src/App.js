5eddb77313ddd4c3cc5e1cc04117cc0223e3149c
igorbr18 <92476751+igorbr18@users.noreply.github.com>
2021-11-01 19:45:55 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import HeaderResponsive from './components/HeaderResponsivo';
+import MainSection from './components/MainSection';
+
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderResponsive />
+      <MainSection/>
+     
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MainSection/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './styles.css';
+function MainSection(){
+return(
+    <div className="main-container">
+        
+    <div className="main-data-container">
+            <h1>Rick and Morty</h1>
+            <p>O brilhante cientista beberrão Rick sequestra Morty, seu neto aborrescente, para viver loucuras em outros mundos e dimensões alternativas.
+            </p>
+            <button type="submit">Veja mais</button>
+        </div>
+
+
+        <img src="https://img.elo7.com.br/product/zoom/2AC1187/placa-decorativa-quadro-anime-rick-and-morty-h347-filme.jpg"
+        alt="rick-morty-img"/>
+
+        
+    </div>
+
+);
+
+}
+
+export default MainSection;
